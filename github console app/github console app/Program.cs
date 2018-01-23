5df78e46da6cbced7a74fcacf7a617d7ce438b88
Devin Kerr <dapperkerr@gmail.com>
2018-01-23 18:37:09 -0500

    added BoomBoom

---
+++ github console app/github console app/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace github_console_app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //testing a comment 
+            int var = 0;
+            string BoomBoom = "Bam";
+        }
+    }
+}
